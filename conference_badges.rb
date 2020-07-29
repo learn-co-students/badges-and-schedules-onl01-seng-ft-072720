c6723cfcffea928e2468580c4580e395f9942251
Evan Pavone <evanrpavone@gmail.com>
2020-07-29 15:54:52 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  names = []
+  array.each do |name|
+    names.push("Hello, my name is #{name}.")
+  end
+  return names
+end
+def assign_rooms(array)
+  names=[]
+  counter = 1
+  array.each do |name|
+    names.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter +=1
+  end
+  return names
+end
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end
+  assign_rooms(array).each do |id|
+    puts id
+  end
+end
