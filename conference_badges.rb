9019c0bc132249f449342c371de3cd7cd2c2ac3c
George Talnagi <gtalnagi@yahoo.com>
2020-07-30 16:23:03 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  new_speakers = []
+  speakers.each do |names|
+    new_speakers.push("Hello, my name is #{names}.")
+  end
+  return new_speakers
+end
+  
